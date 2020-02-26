634a39ac868d784bc57de61e907e216c370fc0d6
Pratik Aher <pratikaher88@gmail.com>
2020-02-26 22:40:25 +0530

    xyz

--- init.py
+++ init.py
@@ -1,8 +1,7 @@
 import os
-# os.environ['CELERY_CONFIG_MODULE'] = 'sdtcrawler.celeryconfig'
-
 
 PORT = os.environ['PORT']
 with io.open("scrapyd.conf", 'r+', encoding='utf-8') as f:
     f.read()
     f.write(u'\nhttp_port = %s\n' % PORT)
+
